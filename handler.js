a6a64b9872dabc451565dfeaecc75b36f96db635
pankaj <pankaj.smartsewa@gmail.com>
2020-12-06 02:36:42 +0530

    first commit

---
+++ handler.js
@@ -0,0 +1,18 @@
+'use strict';
+const moment = require('moment');
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Server time is '+moment.now(),
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
